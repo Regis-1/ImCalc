a88df0e6742a3fc9f541980989c2fce7fb775ef7
Regis-1 <jdudzinski101@gmail.com>
2025-08-02 11:52:15 +0200

    Implemented simple ImGui windows rendering
    It has some basic widgets and controls, but its only the foundation.
    This will be expanded and improved from here.

--- main.cpp
+++ main.cpp
@@ -6,6 +6,15 @@
 
 #include <iostream>
 
+void createMyImGuiWindow(ImVec4 &clearColor, bool &done, ImGuiIO &io) {
+    ImGui::Begin("myWindow");
+    ImGui::ColorEdit3("clearColor", (float*)&clearColor);
+    if (ImGui::Button("Exit"))
+        done = true;
+    ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+    ImGui::End();
+}
+
 int main(int, char**)
 {
     if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMEPAD))
@@ -31,11 +40,28 @@ int main(int, char**)
     SDL_SetWindowPosition(window, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
     SDL_ShowWindow(window);
 
+    // ImGui setup
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO &io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+
+    ImGui::StyleColorsDark();
+    ImGuiStyle &style = ImGui::GetStyle();
+    style.ScaleAllSizes(mainScale);
+    style.FontScaleDpi = mainScale;
+
+    ImGui_ImplSDL3_InitForSDLRenderer(window, renderer);
+    ImGui_ImplSDLRenderer3_Init(renderer);
+
+    ImVec4 clearColor = ImVec4(0.5f, 0.5f, 0.5f, 1.0f);
+
     bool done = false;
     SDL_Event event;
     while (!done) {
         // getting input and events
         while (SDL_PollEvent(&event)) {
+            ImGui_ImplSDL3_ProcessEvent(&event);
             if (event.type == SDL_EVENT_QUIT)
                 done = true;
             if (event.type == SDL_EVENT_WINDOW_CLOSE_REQUESTED && event.window.windowID == SDL_GetWindowID(window))
@@ -47,11 +73,24 @@ int main(int, char**)
             continue;
         }
 
-        SDL_SetRenderScale(renderer, 1.0f, 1.0f);
-        SDL_SetRenderDrawColorFloat(renderer, 0.3f, 0.8f, 0.3f, 1.0f);
+        // ImGui rendering
+        ImGui_ImplSDLRenderer3_NewFrame();
+        ImGui_ImplSDL3_NewFrame();
+        ImGui::NewFrame();
+
+        createMyImGuiWindow(clearColor, done, io);
+
+        ImGui::Render();
+        SDL_SetRenderScale(renderer, io.DisplayFramebufferScale.x, io.DisplayFramebufferScale.y);
+        SDL_SetRenderDrawColorFloat(renderer, clearColor.x, clearColor.y, clearColor.z, clearColor.w);
         SDL_RenderClear(renderer);
+        ImGui_ImplSDLRenderer3_RenderDrawData(ImGui::GetDrawData(), renderer);
         SDL_RenderPresent(renderer);
     }
+
+    ImGui_ImplSDLRenderer3_Shutdown();
+    ImGui_ImplSDL3_Shutdown();
+    ImGui::DestroyContext();
 
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
