c3663b02f7e9e8274269d79d2fb7aef021ce99c8
Regis-1 <jdudzinski101@gmail.com>
2025-08-02 11:18:42 +0200

    Implemented creation of SDL3 window
    Currently this window is blank and without any content, but it has
    SDL3 renderer as a graphic pipeline. This allows to easily create
    ImGui context and all necessary UI.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,61 @@
+#include "imgui.h"
+#include "backends/imgui_impl_sdl3.h"
+#include "backends/imgui_impl_sdlrenderer3.h"
+
+#include <SDL3/SDL.h>
+
+#include <iostream>
+
 int main(int, char**)
 {
+    if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMEPAD))
+    {
+        std::cerr << "Error: SDL_Init(): " << SDL_GetError() << "\n";
+        return -1;
+    }
+
+    float mainScale = SDL_GetDisplayContentScale(SDL_GetPrimaryDisplay());
+    SDL_WindowFlags windowFlags = SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIDDEN | SDL_WINDOW_HIGH_PIXEL_DENSITY;
+    SDL_Window *window = SDL_CreateWindow("My ImGui sdl3 window", 
+            (int)(1280 * mainScale), (int)(720 * mainScale), windowFlags);
+    if (window == nullptr) {
+        std::cerr << "Error SDL_CreateWindow(): " << SDL_GetError() << "\n";
+        return -1;
+    }
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, nullptr);
+    SDL_SetRenderVSync(renderer, 1);
+    if (renderer == nullptr) {
+        std::cerr << "Error SDL_CreateRenderer(): " << SDL_GetError() << "\n";
+        return -1;
+    }
+    SDL_SetWindowPosition(window, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
+    SDL_ShowWindow(window);
+
+    bool done = false;
+    SDL_Event event;
+    while (!done) {
+        // getting input and events
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT)
+                done = true;
+            if (event.type == SDL_EVENT_WINDOW_CLOSE_REQUESTED && event.window.windowID == SDL_GetWindowID(window))
+                done = true;
+        }
+
+        if (SDL_GetWindowFlags(window) & SDL_WINDOW_MINIMIZED) {
+            SDL_Delay(10);
+            continue;
+        }
+
+        SDL_SetRenderScale(renderer, 1.0f, 1.0f);
+        SDL_SetRenderDrawColorFloat(renderer, 0.3f, 0.8f, 0.3f, 1.0f);
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
